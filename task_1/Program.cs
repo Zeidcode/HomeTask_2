d312a6bc32a2c1f2c53c1f878b28e0fe783eac12
ZEID <zeidcode@gmail.com>
2022-08-10 23:02:20 +0400

    ВтороеОТтрехзначного

---
+++ task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше 3-хзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int FrstDig =  a / 100;
+int ScndDig = a % 100;
+int ThrdDig = ScndDig / 10;
+Console.WriteLine(ThrdDig);
