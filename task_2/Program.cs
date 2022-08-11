92d8ab430b05769d6aa844e3c2bb0fb1ddc76cbe
ZEID <zeidcode@gmail.com>
2022-08-11 21:22:49 +0400

    ВыводТретьегоЧислаИегоОтсутствие

---
+++ task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num<=99)
+{
+    Console.WriteLine("Третье число отсутствует!!!");
+}
+
+int digitIndex = 2;
+int digit      = num.ToString()[digitIndex] - '0';
+
+Console.WriteLine(digit);
