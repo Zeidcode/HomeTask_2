c253fac00c7166aa22cc5745a389efc254906b0e
ZEID <zeidcode@gmail.com>
2022-08-11 22:00:16 +0400

    ДнииНедели

---
+++ task_3/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Введите численнно день недели:");
+int n = Convert.ToInt32(Console.ReadLine());
+switch(n)
+{
+case 1:    
+    {
+        Console.WriteLine("НЕТ");
+        break;
+    }
+case 2:
+    {
+     Console.WriteLine("НЕТ");
+     break;   
+    }
+case 3:
+    {
+        Console.WriteLine("НЕТ");
+        break;
+    }
+case 4:
+    {
+        Console.WriteLine("НЕТ");
+        break;
+    }
+case 5:
+    {
+        Console.WriteLine("НЕТ");
+        break;
+    }
+case 6:
+    {
+        Console.WriteLine("ДА");
+        break;
+    }
+case 7:
+    {
+        Console.WriteLine("ДА");
+        break;
+    }
+} 
